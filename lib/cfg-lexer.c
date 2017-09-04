mnjstwins/syslog-ng

commit 484e591e4da79a981dd3a8773e91673d0962b248

config: move plugin autoloading to cfg_new
It was in the lexer for historical reasons, but now we use
the config object just as a container for the loaded
modules. Sidenote: previously the config version was known
during plugin loading, but now the config file will be
parsed after the plugins are loaded.

Signed-off-by: Máté Farkas <mate.farkas@balabit.com>

lib/cfg-lexer.c
@@ -946,7 +946,6 @@ relex:
           msg_warning("WARNING: Configuration file has no version number, assuming syslog-ng 2.1 format. Please add @version: maj.min to the beginning of the file to indicate this explicitly");
           cfg_set_version(configuration, 0x0201);
         }
-      cfg_load_candidate_modules(configuration);
       self->non_pragma_seen = TRUE;
     }
 

lib/cfg.c
@@ -326,6 +326,7 @@ cfg_new(gint version)
 
   cfg_tree_init_instance(&self->tree, self);
   cfg_register_builtin_plugins(self);
+  cfg_load_candidate_modules(self);
   return self;
 }
 
@@ -373,10 +374,7 @@ cfg_run_parser(GlobalConfig *self, CfgLexer *lexer, CfgParser *parser, gpointer 
 void
 cfg_load_candidate_modules(GlobalConfig *self)
 {
-  if (!self->candidate_plugins)
-    {
-      plugin_load_candidate_modules(self);
-    }
+  plugin_load_candidate_modules(self);
 
 #if (!SYSLOG_NG_ENABLE_FORCED_SERVER_MODE)
   if (!plugin_load_module("license", self, NULL))

commit f1ad35349b100fd50557711c055631551a03889c

cfg: restore module-path functionality
Patch 484e591e4d broke one functionality, namely the ability to customize
the module-path that controls where modules are loaded from. This patch
restores that. An example is:

@define module-path '/usr/lib/syslog-ng:/usr/local/lib/syslog-ng'

Once this setting is defined right after the version directive (e.g. before
actual config statements), syslog-ng would scan both /usr/lib
and /usr/local/lib to find modules.

If using cfg_new_snippet(), module autoloading is still disabled, but
with a boolean variable, instead of bypassing the call during the
constructor.

Signed-off-by: Balazs Scheidler <balazs.scheidler@balabit.com>

lib/cfg-lexer.c
@@ -949,6 +949,8 @@ relex:
           cfg_set_version(configuration, 0x0201);
         }
 
+      cfg_load_candidate_modules(configuration);
+
 #if (!SYSLOG_NG_ENABLE_FORCED_SERVER_MODE)
       if (!plugin_load_module("license", configuration, NULL))
         {

lib/cfg.c
@@ -188,8 +188,14 @@ cfg_load_module(GlobalConfig *cfg, const gchar *module_name)
 void
 cfg_load_candidate_modules(GlobalConfig *self)
 {
-  _sync_plugin_module_path_with_global_define(self);
-  plugin_load_candidate_modules(&self->plugin_context);
+  gboolean autoload_enabled = atoi(cfg_args_get(self->lexer->globals, "autoload-compiled-modules") ? : "1");
+
+  if (self->use_plugin_discovery &&
+      autoload_enabled)
+    {
+      _sync_plugin_module_path_with_global_define(self);
+      plugin_load_candidate_modules(&self->plugin_context);
+    }
 }
 
 Plugin *
@@ -377,7 +383,7 @@ cfg_register_builtin_plugins(GlobalConfig *self)
 }
 
 GlobalConfig *
-cfg_new_snippet(gint version)
+cfg_new(gint version)
 {
   GlobalConfig *self = g_new0(GlobalConfig, 1);
 
@@ -418,16 +424,18 @@ cfg_new_snippet(gint version)
 
   cfg_tree_init_instance(&self->tree, self);
   plugin_context_init_instance(&self->plugin_context);
+  self->use_plugin_discovery = TRUE;
 
   cfg_register_builtin_plugins(self);
   return self;
 }
 
 GlobalConfig *
-cfg_new(gint version)
+cfg_new_snippet(gint version)
 {
-  GlobalConfig *self = cfg_new_snippet(version);
-  cfg_load_candidate_modules(self);
+  GlobalConfig *self = cfg_new(version);
+
+  self->use_plugin_discovery = FALSE;
   return self;
 }
 

lib/cfg.h
@@ -67,7 +67,7 @@ struct _GlobalConfig
   gint parsed_version;
   const gchar *filename;
   PluginContext plugin_context;
-  gboolean autoload_compiled_modules;
+  gboolean use_plugin_discovery;
   CfgLexer *lexer;
 
   StatsOptions stats_options;